2d1877c3c47e4ea70cfb405895211df83eca6d9a
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:25:08 +0600

    improve

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
 
-async function SaveError(err) {
-    const url = 'https://worker-curly-math-37b8.techzbots1.workers.dev/rM8kBk5lzLropzqxZsaxc3L5ndgDzJ21t7lLreY5yG7sGRj2TH'
-
-    err = 'from animedexapi: ' + err
-    await fetch(url, { headers: { text: err } })
-
+interface SaveErrorProps {
+  error: string;
 }
 
-export { SaveError }
+const saveError = async ({ error }: SaveErrorProps): Promise<void> => {
+  const url = 'https://worker-curly-math-37b8.techzbots1.workers.dev/rM8kBk5lzLropzqxZsaxc3L5ndgDzJ21t7lLreY5yG7sGRj2TH';
+
+  try {
+    await axios.post(url, { error: `from animedexapi: ${error}` });
+  } catch (error) {
+    console.error(`Error saving error to server: ${error}`);
+  }
+};
+
+export default saveError;
