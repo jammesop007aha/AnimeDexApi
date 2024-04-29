019d3b1221907f496977632928e107bd0e656a58
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:25:13 +0600

    improve

--- src/gogo_extractor.js
+++ src/gogo_extractor.js
@@ -8,31 +8,47 @@ const keys = {
 
 /**
  * Parses the embedded video URL to encrypt-ajax.php parameters
- * @param {cheerio} $ Cheerio object of the embedded video page
+ * @param {cheerio.CheerioAPI} $ Cheerio object of the embedded video page
  * @param {string} id Id of the embedded video URL
+ * @returns {Promise<string>}
  */
-export async function generateEncryptAjaxParameters($, id) {
-    // encrypt the key
-    const encrypted_key = CryptoJS.AES['encrypt'](id, keys.key, {
-        iv: keys.iv,
-    });
+export async function generateEncryptAjaxParameters($: cheerio.CheerioAPI, id: string): Promise<string> {
+    try {
+        // encrypt the key
+        const encrypted_key = CryptoJS.AES.encrypt(id, keys.key, {
+            iv: keys.iv,
+        }).toString();
 
-    const script = $("script[data-name='episode']").data().value;
-    const token = CryptoJS.AES['decrypt'](script, keys.key, {
-        iv: keys.iv,
-    }).toString(CryptoJS.enc.Utf8);
+        const script = $("script[data-name='episode']").data().value;
+        const token = CryptoJS.AES.decrypt(script, keys.key, {
+            iv: keys.iv,
+        }).toString(CryptoJS.enc.Utf8);
 
-    return 'id=' + encrypted_key + '&alias=' + id + '&' + token;
+        return `id=${encrypted_key}&alias=${id}&${token}`;
+    } catch (error) {
+        console.error('Error generating encrypt-ajax parameters:', error);
+        throw error;
+    }
 }
+
 /**
  * Decrypts the encrypted-ajax.php response
  * @param {object} obj Response from the server
+ * @returns {any}
  */
-export function decryptEncryptAjaxResponse(obj) {
-    const decrypted = CryptoJS.enc.Utf8.stringify(
-        CryptoJS.AES.decrypt(obj.data, keys.second_key, {
-            iv: keys.iv,
-        })
-    );
-    return JSON.parse(decrypted);
+export function decryptEncryptAjaxResponse(obj: { data: string }): any {
+    try {
+        const decrypted = CryptoJS.enc.Utf8.stringify(
+            CryptoJS.AES.decrypt(obj.data, keys.second_key, {
+                iv: keys.iv,
+            })
+        );
+        return JSON.parse(decrypted);
+    } catch (error) {
+        console.error('Error decrypting encrypt-ajax response:', error);
+        throw error;
+    }
 }
+
+
+npm install crypto-js cheerio
