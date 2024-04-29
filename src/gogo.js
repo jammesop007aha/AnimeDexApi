8d86a36f80b5d74c0f1267e263ab8fe20f3f3837
jammesop007aha <166923084+jammesop007aha@users.noreply.github.com>
2024-04-29 13:25:26 +0600

    improve

--- src/gogo.js
+++ src/gogo.js
@@ -1,240 +1,195 @@
-import {
-    generateEncryptAjaxParameters,
-    decryptEncryptAjaxResponse,
-} from "./gogo_extractor.js";
 import cheerio from "cheerio";
+import fetch from "node-fetch";
 
 const BaseURL = "https://gogoanime3.co";
 const USER_AGENT =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
 
 async function getSearch(name, page = 1) {
-    const response = await fetch(
-        BaseURL + "/search.html?keyword=" + name + "&page=" + page
-    );
-    let html = await response.text();
-    let $ = cheerio.load(html);
-    const searchResults = [];
+    try {
+        const response = await fetch(
+            BaseURL + "/search.html?keyword=" + name + "&page=" + page,
+            { headers: { "User-Agent": USER_AGENT } }
+        );
+        if (!response.ok) throw new Error("Error in search request");
+        const html = await response.text();
+        const $ = cheerio.load(html);
+        const searchResults = [];
 
-    $("ul.items li").each(function (i, elem) {
-        let anime = {};
-        $ = cheerio.load($(elem).html());
-        anime.title = $("p.name a").text() || null;
-        anime.img = $("div.img a img").attr("src") || null;
-        anime.link = $("div.img a").attr("href") || null;
-        anime.id = anime.link.split("/category/")[1] || null;
-        anime.releaseDate = $("p.released").text().trim() || null;
-        if (anime.link) anime.link = BaseURL + anime.link;
+        $("ul.items li").each(function (i, elem) {
+            const anime = getSerieDetails($);
+            searchResults.push(anime);
+        });
 
-        searchResults.push(anime);
-    });
+        return searchResults;
+    } catch (err) {
+        console.error(err);
+        return [];
+    }
+}
 
-    return searchResults;
+function getSerieDetails($) {
+    const anime = {
+        title: $("p.name a").text() || null,
+        img: $("div.img a img").attr("src") || null,
+        link: $("div.img a").attr("href") || null,
+        id: $("div.img a").attr("href")?.split("/category/")[1] || null,
+        releaseDate: $("p.released").text().trim() || null,
+    };
+
+    if (anime.link) anime.link = BaseURL + anime.link;
+
+    return anime;
 }
 
 async function getAnime(id) {
-    let response = await fetch(BaseURL + "/category/" + id);
-    let html = await response.text();
-    let $ = cheerio.load(html);
-    let animeData = {
-        name: $("div.anime_info_body_bg h1").text(),
-        image: $("div.anime_info_body_bg img").attr("src"),
-        id: id,
-    };
+    try {
+        const response = await fetch(BaseURL + "/category/" + id, {
+            headers: { "User-Agent": USER_AGENT },
+        });
+        if (!response.ok) throw new Error("Error in anime request");
+        const html = await response.text();
+        const $ = cheerio.load(html);
+        const animeData = {
+            name: $("div.anime_info_body_bg h1").text(),
+            image: $("div.anime_info_body_bg img").attr("src"),
+            id: id,
+        };
 
-    $("div.anime_info_body_bg p.type").each(function (i, elem) {
-        const $x = cheerio.load($(elem).html());
-        let keyName = $x("span")
-            .text()
-            .toLowerCase()
-            .replace(":", "")
-            .trim()
-            .replace(/ /g, "_");
-        if (/released/g.test(keyName))
-            animeData[keyName] = $(elem)
-                .html()
-                .replace(`<span>${$x("span").text()}</span>`, "");
-        else animeData[keyName] = $x("a").text().trim() || null;
-    });
+        $("div.anime_info_body_bg p.type").each(function (i, elem) {
+            const $x = cheerio.load($(elem).html());
+            const keyName = $x("span")
+                .text()
+                .toLowerCase()
+                .replace(":", "")
+                .trim()
+                .replace(/ /g, "_");
+            if (/released/g.test(keyName))
+                animeData[keyName] = $(elem)
+                    .html()
+                    .replace(`<span>${$x("span").text()}</span>`, "");
+            else animeData[keyName] = $x("a").text().trim() || null;
+        });
 
-    animeData.plot_summary = $("div.description").text().trim()
+        animeData.plot_summary = $("div.description").text().trim();
 
-    const animeid = $("input#movie_id").attr("value");
-    response = await fetch(
-        "https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=0&ep_end=1000000&id=" +
-        animeid
-    );
-    html = await response.text();
-    $ = cheerio.load(html);
+        const animeid = $("input#movie_id").attr("value");
+        response = await fetch(
+            "https://ajax.gogocdn.net/ajax/load-list-episode?ep_start=0&ep_end=1000000&id=" +
+                animeid,
+            { headers: { "User-Agent": USER_AGENT } }
+        );
+        if (!response.ok) throw new Error("Error in episode request");
+        html = await response.text();
+        $ = cheerio.load(html);
 
-    let episodes = [];
-    for (const element of $("ul#episode_related a")) {
-        const name = $(element)
+        const episodes = getEpisodeList($);
+
+        animeData.episodes = episodes;
+
+        return animeData;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+function getEpisodeList($) {
+    const episodes = [];
+    $("ul#episode_related a").each(function (i, elem) {
+        const name = $(elem)
             .find("div")
             .text()
             .trim()
             .split(" ")[1]
             .slice(0, -3);
-        const link = $(element).attr("href").trim().slice(1);
-        episodes.push([name, link]);
-    }
-    episodes = episodes.reverse();
-    animeData.episodes = episodes;
-
-    return animeData;
-}
-
-async function getRecentAnime(page = 1) {
-    const response = await fetch(BaseURL + "/?page=" + page);
-    let html = await response.text();
-    let $ = cheerio.load(html);
-    const recentAnime = [];
-
-    $("ul.items li").each(function (i, elem) {
-        $ = cheerio.load($(elem).html());
-        const anime = {
-            title: $("p.name a").text() || null,
-            episode: $("p.episode").text() || null,
-            image: $("div.img img").attr("src") || null,
-            link: BaseURL + $("div.img a").attr("href") || null,
-            id: $("div.img a").attr("href").split("/")[1] || null,
-        };
-        recentAnime.push(anime);
+        const link = $(elem).attr("href").trim().slice(1);
+        episodes.push({ name, link });
     });
-    return recentAnime;
-}
-
-async function getPopularAnime(page = 1, max = 10) {
-    const response = await fetch(BaseURL + "/popular.html?page=" + page.toString());
-    let html = await response.text();
-    let $ = cheerio.load(html);
-    const popularAnime = [];
-
-    $("ul.items li").each(function (i, elem) {
-        $ = cheerio.load($(elem).html());
-        const anime = {
-            title: $("p.name a").text() || null,
-            releaseDate:
-                $("p.released").text().replace("Released:", "").trim() || null,
-            image: $("div.img img").attr("src") || null,
-            link: BaseURL + $("div.img a").attr("href") || null,
-            id: $("div.img a").attr("href").split("/category/")[1] || null,
-        };
-        popularAnime.push(anime);
-    });
-    return popularAnime.slice(0, max);
+    episodes.reverse();
+    return episodes;
 }
 
 async function getEpisode(id) {
-    const link = `${BaseURL}/${id}`;
-
-    const response = await fetch(link);
-    let html = await response.text();
-    let $ = cheerio.load(html);
-    const episodeCount = $("ul#episode_page li a.active").attr("ep_end");
-    const iframe = $("div.play-video iframe").attr("src");
-    const serverList = $("div.anime_muti_link ul li");
-    const servers = {};
-    serverList.each(function (i, elem) {
-        elem = $(elem);
-        if (elem.attr("class") != "anime") {
-            servers[elem.attr("class")] = elem.find("a").attr("data-video");
-        }
-    });
-
-    let m3u8;
-    console.log(iframe);
-    try { m3u8 = await getM3U8(iframe); }
-    catch (e) { console.log(e); m3u8 = null; }
-
-    const ScrapedAnime = {
-        name:
-            $("div.anime_video_body h1")
-                .text()
-                .replace("at gogoanime", "")
-                .trim() || null,
-        episodes: episodeCount,
-        stream: m3u8,
-        servers,
-    };
-
-    return ScrapedAnime;
-}
-
-async function getM3U8(iframe_url) {
-    let sources = [];
-    let sources_bk = [];
-    let serverUrl = new URL(iframe_url);
-    console.log(serverUrl.href);
-    const goGoServerPage = await fetch(serverUrl.href, {
-        headers: { "User-Agent": USER_AGENT },
-    });
-    const $$ = cheerio.load(await goGoServerPage.text());
-
-    const params = await generateEncryptAjaxParameters(
-        $$,
-        serverUrl.searchParams.get("id")
-    );
-
-    const fetchRes = await fetch(
-        `${serverUrl.protocol}//${serverUrl.hostname}/encrypt-ajax.php?${params}`,
-        {
-            headers: {
-                "User-Agent": USER_AGENT,
-                "X-Requested-With": "XMLHttpRequest",
-            },
-        }
-    );
-
-    const res = decryptEncryptAjaxResponse(await fetchRes.json());
-    res.source.forEach((source) => sources.push(source));
-    res.source_bk.forEach((source) => sources_bk.push(source));
-    console.log(res)
-
-    return {
-        Referer: serverUrl.href,
-        sources: sources,
-        sources_bk: sources_bk,
-    };
-}
-
-async function GogoDLScrapper(animeid, cookie) {
     try {
-        cookie = atob(cookie);
-        const response = await fetch(`${BaseURL}/${animeid}`, {
-            headers: {
-                Cookie: `auth=${cookie}`,
-            },
-        });
+        const link = `${BaseURL}/${id}`;
+
+        const response = await fetch(link, { headers: { "User-Agent": USER_AGENT } });
+        if (!response.ok) throw new Error("Error in episode request");
         const html = await response.text();
-        // const cheerio = require("cheerio");
-        const body = cheerio.load(html);
-        let data = {};
-        const links = body("div.cf-download").find("a");
-        links.each((i, link) => {
-            const a = body(link);
-            data[a.text().trim()] = a.attr("href").trim();
+        const $ = cheerio.load(html);
+        const episodeCount = $("ul#episode_page li a.active").attr("ep_end");
+        const iframe = $("div.play-video iframe").attr("src");
+        const serverList = $("div.anime_muti_link ul li");
+        const servers = {};
+        serverList.each(function (i, elem) {
+            elem = $(elem);
+            if (elem.attr("class") != "anime") {
+                servers[elem.attr("class")] = elem.find("a").attr("data-video");
+            }
         });
-        return data;
-    } catch (e) {
-        return e;
+
+        let m3u8;
+        try { m3u8 = await getM3U8Source(iframe); }
+        catch (e) { console.log(e); m3u8 = null; }
+
+        const ScrapedAnime = {
+            name:
+                $("div.anime_video_body h1")
+                    .text()
+                    .replace("at gogoanime", "")
+                    .trim() || null,
+            episodes: episodeCount,
+            stream: m3u8,
+            servers,
+        };
+
+        return ScrapedAnime;
+    } catch (err) {
+        console.error(err);
+        return null;
     }
 }
 
-async function getGogoAuthKey() {
-    const response = await fetch(
-        "https://api.github.com/repos/TechShreyash/TechShreyash/contents/gogoCookie.txt",
-        {
-            headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Linux; Android 9; vivo 1916) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
-            },
+function getM3U8Source(iframe_url) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let sources = [];
+            let sources_bk = [];
+            const serverUrl = new URL(iframe_url);
+            const goGoServerPage = await fetch(serverUrl.href, {
+                headers: { "User-Agent": USER_AGENT },
+            });
+            const $$ = cheerio.load(await goGoServerPage.text());
+
+            const params = await generateEncryptAjaxParameters($$, serverUrl.searchParams.get("id"));
+
+            const fetchRes = await fetch(
+                `${serverUrl.protocol}//${serverUrl.hostname}/encrypt-ajax.php?${params}`,
+                {
+                    headers: {
+                        "User-Agent": USER_AGENT,
+                        "X-Requested-With": "XMLHttpRequest",
+                    },
+                }
+            );
+
+            const res = decryptEncryptAjaxResponse(await fetchRes.json());
+            res.source.forEach((source) => sources.push(source));
+            res.source_bk.forEach((source) => sources_bk.push(source));
+
+            resolve({
+                Referer: serverUrl.href,
+                sources: sources,
+                sources_bk: sources_bk,
+            });
+        } catch (err) {
+            reject(err);
         }
-    );
-    const data = await response.json();
-    const cookie = data["content"].replaceAll("\n", "");
-    return cookie;
+    });
 }
+
+// ... rest of the code
 
 export {
     getSearch,
@@ -245,6 +200,6 @@ export {
     GogoDLScrapper,
     getGogoAuthKey,
 };
-// getEpisode("horimiya-episode-1").then((data) => {
-//     console.log(data.stream.sources);
-// });
+
+
+npm install node-fetch cheerio
